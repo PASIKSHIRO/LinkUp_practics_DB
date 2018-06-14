4131e4298741145cd620e3f6f8570839968adcff
PASIKSHIRO <shiropasik@gmail.com>
2018-06-14 17:55:07 +0300

    Associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :response, polymorphic: true
-  validate :cannot_leave_review_for_an_item, :cannot_leave_review_for_a_user,
-           on: :create
 end
