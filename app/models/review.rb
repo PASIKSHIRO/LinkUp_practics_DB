b26c6bd675b1b8d56f39cfdf8ffa91eeabbacf14
PASIKSHIRO <shiropasik@gmail.com>
2018-06-14 16:39:46 +0300

    Associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :response, polymorphic: true
+  validate :cannot_leave_review_for_an_item, :cannot_leave_review_for_a_user,
+           on: :create
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :items, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, as: :response, dependent: :destroy
 end
