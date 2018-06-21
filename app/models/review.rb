8de5217a34c32f36872d436780d70562e58d0d3d
PASIKSHIRO <shiropasik@gmail.com>
2018-06-21 14:47:55 +0300

    Task_3

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,22 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :response, polymorphic: true
+
+  def leave_user
+    return unless response_type == 'User'
+    booking = Booking.joins(:item).where(items: { user_id: response_id }, bookings: { user_id: user_id })
+    renting = Booking.joins(:item).where(items: { user_id: user_id }, bookings: { user_id: response_id})
+    if booking.empty? && renting.empty?
+      errors.add(:user_id, 'Cannot leave a review for a user you never dealt with')
+    end
+  end
+
+  def leave_item
+    return unless response_type == 'Item'
+    booking = Booking.where(bookings: { user_id: user_id, item_id: response_id })
+    if booking.empty?
+    errors.add(:user_id, 'Cannot leave a review for an item you never booked')
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_many :reviews, as: :response, dependent: :destroy
+  belongs_to :city
+  has_many :reviews_items_reviews, through: :items, source: :reviews
 end
